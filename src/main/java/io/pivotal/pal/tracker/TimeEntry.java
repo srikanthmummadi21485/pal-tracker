1dcf245dc576de2dae2e93d97513433cba6c77c9
pal_user <srikanth_mummadi@syntelinc.com>
2018-05-22 11:16:53 -0600

    Updated time tracker API

--- src/main/java/io/pivotal/pal/tracker/TimeEntry.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntry.java
@@ -1,6 +1,7 @@
 package io.pivotal.pal.tracker;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 public class TimeEntry {
 
@@ -13,9 +14,9 @@ public class TimeEntry {
     public TimeEntry() {
     }
 
-    public TimeEntry(long id, long projectId, LocalDate date, int hours) {
-        this.id = id;
+    public TimeEntry(long projectId, long userId, LocalDate date, int hours) {
         this.projectId = projectId;
+        this.userId = userId;
         this.date = date;
         this.hours = hours;
     }
@@ -66,5 +67,34 @@ public class TimeEntry {
 
     public void setHours(int hours) {
         this.hours = hours;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TimeEntry timeEntry = (TimeEntry) o;
+        return id == timeEntry.id &&
+                projectId == timeEntry.projectId &&
+                userId == timeEntry.userId &&
+                hours == timeEntry.hours &&
+                Objects.equals(date, timeEntry.date);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id, projectId, userId, date, hours);
+    }
+
+    @Override
+    public String toString() {
+        return "TimeEntry{" +
+                "id=" + id +
+                ", projectId=" + projectId +
+                ", userId=" + userId +
+                ", date=" + date +
+                ", hours=" + hours +
+                '}';
     }
 }
